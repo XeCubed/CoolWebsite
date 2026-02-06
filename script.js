af99cb3568b06d72cb486f4aa6fda07687ee9f42
-Xenon+ <mousegamer942@gmail.com>
2026-02-06 08:17:07 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const button=document.getElementById("btn");
+
+const gif=document.getElementById("gif");
+
+let go=true
+
+button.addEventListener("click", function() {
+    if (go===true) {
+        gif.style.display="block";
+
+        go=false
+        setTimeout(() => {
+          gif.style.display="none";
+          go=true
+        },2500);
+    }
+});
